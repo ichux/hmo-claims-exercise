6fe06918d5291655107762dc01b7437ab1107e65
Chukwudi Nwachukwu <theichux@gmail.com>
2025-01-23 09:56:13 +0100

    made some styles

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,27 @@ from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
 
 application = Flask(
-        import_name="app",
-        template_folder="templates",
-        static_folder='static',
-        instance_relative_config=True)
+    import_name="app",
+    template_folder="templates",
+    static_folder="static",
+    instance_relative_config=True,
+)
 
 database = SQLAlchemy(application)
 
+
 def create_app(config_name):
     application.config.from_object(app_config[config_name])
-    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+    application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     database.init_app(application)
 
-    migrate = Migrate(app = application, db = database, directory="intron_health_migrations", render_as_batch=True)
+    migrate = Migrate(
+        app=application,
+        db=database,
+        directory="intron_health_migrations",
+        render_as_batch=True,
+    )
 
     register_blueprints()
 
@@ -29,7 +36,10 @@ def create_app(config_name):
 """
  The following registers the Blueprints with the application.
 """
+
+
 def register_blueprints():
     # Import all BluePrints
     from .home import home as home_blueprint
-    application.register_blueprint(home_blueprint, url_prefix='/home')
+
+    application.register_blueprint(home_blueprint, url_prefix="/home")
--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, SelectField, TextAreaField, IntegerField, DateField, DateTimeField
-from wtforms import validators
+from wtforms import (
+    DateField,
+    DateTimeField,
+    IntegerField,
+    RadioField,
+    SelectField,
+    StringField,
+    TextAreaField,
+    validators,
+)
+
 from app.models import User
 
 all_users = User.query.with_entities(User.name).all()
 
 
 class AddUser(FlaskForm):
-    name = StringField('name', validators=[validators.length(min=3, max=100), validators.DataRequired()])
-    gender = RadioField('gender', choices = ['male', 'female'])
-    salary = IntegerField(validators=[validators.NumberRange(min=0), validators.DataRequired()])
-    date_of_birth = DateField(format='%Y-%m-%d', validators=[validators.DataRequired()])
+    name = StringField(
+        "name",
+        validators=[validators.length(min=3, max=100), validators.DataRequired()],
+    )
+    gender = RadioField("gender", choices=["male", "female"])
+    salary = IntegerField(
+        validators=[validators.NumberRange(min=0), validators.DataRequired()]
+    )
+    date_of_birth = DateField(format="%Y-%m-%d", validators=[validators.DataRequired()])
